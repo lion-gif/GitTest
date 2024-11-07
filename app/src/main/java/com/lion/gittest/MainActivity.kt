e0839044713361f0c4c8ed47e9acba1d5da075f9
lecture <yunjaeseong429@gmail.com>
2024-11-07 11:34:34 +0900

    Init Commit100

--- app/src/main/java/com/lion/gittest/MainActivity.kt
+++ app/src/main/java/com/lion/gittest/MainActivity.kt
@@ -23,5 +23,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        println("aaaaaaaaaaaa")
     }
 }
