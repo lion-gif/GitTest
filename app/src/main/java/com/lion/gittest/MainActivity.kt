30a4d57c388504f0317722f5ca0c011a05f43df3
lecture <yunjaeseong429@gmail.com>
2024-11-07 11:38:27 +0900

    Init Commit600

--- app/src/main/java/com/lion/gittest/MainActivity.kt
+++ app/src/main/java/com/lion/gittest/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.lion.gittest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lion.gittest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val a1 = 10 + 20
+
+        Log.d("test100", "aaaaa")
     }
 }
